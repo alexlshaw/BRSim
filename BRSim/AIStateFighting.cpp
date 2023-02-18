515dafb0622b735589bdfac2f006aaf2c29dd28d
alexlshaw <alexshaw255@gmail.com>
2023-02-18 19:43:36 +1300

    Target preferences
    When faced with multiple targets, agents now prioritize the one requiring the least rotation to face.

--- BRSim/AIStateFighting.cpp
+++ BRSim/AIStateFighting.cpp
@@ -17,7 +17,7 @@ void AIStateFighting::execute(Agent& owner, const Game& gameState, float frameTi
 	//2. We have 1+ opponents, do we want this fight?
 	if (owner.currentHealth > owner.aiWeights.fleeHealthThreshold || gameState.circleRadius <= owner.aiWeights.stopFleeingCircleThreshold)
 	{
-		Agent& other = owner.otherVisibleAgents[0];
+		Agent& other = determinePreferredTarget(owner);
 		glm::vec2 shootAt = owner.computeTargetInterceptionPoint(other.position, other.currentVelocity());
 		if (owner.rotateTowards(shootAt, frameTime))
 		{
@@ -42,4 +42,30 @@ void AIStateFighting::execute(Agent& owner, const Game& gameState, float frameTi
 		return;	//Switching state deletes the state object, so we want to wrap up here
 	}
 	
+}
+
+//Of the agents we can see, which one do we want to shoot most? For now, just the one that requires the least rotation to face
+Agent& AIStateFighting::determinePreferredTarget(const Agent& owner)
+{
+	float closestIndex = -1;
+	float closestAngle = glm::pi<float>();
+
+	for (int i = 0; i < owner.otherVisibleAgents.size(); i++) {
+		glm::vec2 toHostile = owner.otherVisibleAgents[i].get().position - owner.position;
+		float distance = glm::length(toHostile);
+
+		if (distance < 0.001f) {
+			continue; // ignore entities at the same position as the entity
+		}
+
+		toHostile /= distance;
+
+		float angle = glm::angle(owner.forward(), toHostile);
+
+		if (angle < closestAngle) {
+			closestAngle = angle;
+			closestIndex = i;
+		}
+	}
+	return owner.otherVisibleAgents[closestIndex];
 }
